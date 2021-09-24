7ac0c6fbcc7abc2af2b826a41d291e6186ee8103
masutaro99 <maskenpa1001@gmail.com>
2021-09-24 18:21:12 +0900

    feature: StaticPageコントローラを作成した

---
+++ app/controllers/static_pages_controller.rb
@@ -0,0 +1,10 @@
+class StaticPagesController < ApplicationController
+  def home
+    users = { id:1, nickname: "ぴよっち", age: 22 }
+    render json: users
+  end
+  def help
+    helps = { id:1, nickname: "お助け" }
+    render json: helps
+  end
+end
